7cdd42489b8651d1fd8fc731d9c5590124ce5fbc
CEO-Niels <nielsbeaumont.etu@gmail.com>
2024-06-18 10:22:15 +0200

    truncate title card game

--- resources/views/components/cards/Card-game.blade.php
+++ resources/views/components/cards/Card-game.blade.php
@@ -2,6 +2,6 @@
 <div class="w-full max-w-xs sm:max-w-[12rem] mx-2 rounded-lg overflow-hidden shadow-lg m-auto">
     <img src="{{ $imageUrl }}" alt="{{ $title }}" class="w-full h-48 object-cover" style="border-radius: 1rem 1rem 0 0;">
     <div class="bg-[#000116] text-white text-center py-4" style="border-radius: 0 0 1rem 1rem;">
-        <h2 class="text-lg font-semibold">{{ $title }}</h2>
+        <h2 class="text-lg font-semibold truncate px-5">{{ $title }}</h2>
     </div>
 </div>
