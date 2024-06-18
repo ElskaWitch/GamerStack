357c5f74c929bdf12d3b22b7dfe23f3df1c5f80d
CEO-Niels <nielsbeaumont.etu@gmail.com>
2024-06-18 15:22:14 +0200

    design editeur de text

--- resources/views/pages/note/create.blade.php
+++ resources/views/pages/note/create.blade.php
@@ -4,15 +4,41 @@
     <div class="container mx-auto p-4 flex flex-col items-center">
         <h1 class="text-3xl font-bold text-white mb-4">{{ $gameTitle }}</h1>
     </div>
+    <!--TEXTAREA -->
     <div class="container mx-auto p-4 flex flex-col items-center">
         <div class="bg-[#000116] text-white p-8 rounded-lg shadow-lg mb-4" style="min-width: 60%;">
             <form action="{{ route('note.store') }}" method="POST">
                 @csrf
-                <textarea name="message" class="w-full h-64 bg-transparent text-white rounded-lg p-4 focus:outline-none resize-none" placeholder="Écrivez votre note ici..." style="min-width: 100%;"></textarea>
+                <textarea name="message" class="w-full h-64 bg-transparent text-white rounded-lg p-4 focus:outline-none resize-none" placeholder="Écrivez votre message ici..." style="min-width: 100%;"></textarea>
                 <button type="submit" class="submit mt-4 mx-auto">Valider</button>
             </form>
         </div>
+         <!-- Interface de l'éditeur -->
+         <div class="w-1/4 bg-[#000116] text-white p-4 rounded-lg mr-4">
+            <!-- Styles du texte -->
+            <div class="mb-4">
+                <div class="text-xl font-bold mb-2">Styles du Texte</div>
+                <button class="text-white bg-transparent border border-white rounded px-2 py-1 hover:bg-white hover:text-[#000116] hover:border-transparent" onclick="execCommand('bold')">
+                    Gras
+                </button>
+                <button class="text-white bg-transparent border border-white rounded px-2 py-1 hover:bg-white hover:text-[#000116] hover:border-transparent" onclick="execCommand('italic')">
+                    Italique
+                </button>
+                <button class="text-white bg-transparent border border-white rounded px-2 py-1 hover:bg-white hover:text-[#000116] hover:border-transparent" onclick="execCommand('underline')">
+                    Souligné
+                </button>
+                <input type="color" id="colorPicker" class="ml-2" onchange="execCommand('foreColor', this.value)" title="Changer la couleur du texte">
+            </div>
     </div>
+
+    <script>
+        // Fonction pour exécuter des commandes sur le texte
+        function execCommand(command, value = null) {
+            document.execCommand(command, false, value);
+            // Focus sur la zone de texte après l'exécution de la commande
+            document.getElementById('editor').focus();
+        }
+    </script>
 </x-app-layout>
 
 <style>
